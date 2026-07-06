abd672c0f2d61b82e5443c906c6cbd4b23090f8f
arya <arya.lekshmi@daep.ae>
2026-07-06 12:11:22 +0400

    resolve error off

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -23,7 +23,8 @@ export default defineConfig(
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 			'no-undef': 'off',
-			'@typescript-eslint/no-explicit-any': 'off'
+			'@typescript-eslint/no-explicit-any': 'off',
+			'eslint-disable-next-line svelte/no-navigation-without-resolve':'off'
 		}
 	},
 	{
@@ -33,7 +34,7 @@ export default defineConfig(
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
-				svelteConfig
+				svelteConfig,
 			}
 		}
 	}
